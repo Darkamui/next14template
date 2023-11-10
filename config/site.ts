7d9a9b54821cb760ca4eb9a53ee66af7b055e53b
Daniel Jérusalmy <djerusal@uqac.ca>
2023-11-09 20:52:22 -0500

    initial

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "NextJS Template",
+  description: "NextJS Template w/ shadcn-ui",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com",
+    github: "https://github.com",
+    docs: "https://ui.shadcn.com",
+  },
+};
